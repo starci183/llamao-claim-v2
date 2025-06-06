cb1b925bb6708f97e471525a02e96347c9271fa7
minhnhat166 <minhnhatphan2003@gmail.com>
2025-06-07 00:56:15 +0700

    feat: Refactor Showcase and MainLayout components for improved structure and memoization

--- src/app/showcase/page.tsx
+++ src/app/showcase/page.tsx
@@ -1,5 +1,13 @@
-import React from "react";
+import MainLayout from "@/components/layouts/main-layout";
 
 export default function Showcase() {
-  return <div>Showcase</div>;
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <MainLayout
+        headerIcon="/gifs/llamao_majestic_run.gif"
+        text="Showcase"
+        subHeader={false}
+      ></MainLayout>
+    </div>
+  );
 }
