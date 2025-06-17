4ab83231ec1025a2cab85ac54c640f8cf340df7e
minhnhat166 <minhnhatphan2003@gmail.com>
2025-06-17 23:56:09 +0700

    feat: update About page layout and spacing, add new Mint and MintPlaybook pagesstyle: add custom font faces and utility classes in globals.cssfix: import new font variables in layout.tsxfeat: implement Mint page with image and button componentschore: add framer-motion and related dependencies to yarn.lock

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,4 @@
-import { w95fa } from "@/lib/fonts";
+import { ppMondwest, ppNeueBit, w95fa } from "@/lib/fonts";
 import RootProviders from "@/providers/providers";
 import type { Metadata } from "next";
 import "./globals.css";
@@ -15,7 +15,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body className={` ${w95fa.variable} ${w95fa.className} antialiased`}>
+      <body
+        className={`${w95fa.variable} ${ppNeueBit.variable} ${ppMondwest.variable} ${w95fa.className} antialiased`}
+      >
         <RootProviders>{children}</RootProviders>
       </body>
     </html>
