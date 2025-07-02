0d35096b6447ae13a0db8c40ad9735db3858e178
starci183 <cuongnvtse160875@gmail.com>
2025-07-02 14:52:43 +0700

    123

--- src/app/showcase/page.tsx
+++ src/app/showcase/page.tsx
@@ -1,51 +1,83 @@
 "use client";
 
+import { useState } from "react";
+import MainLayout from "@/components/layouts/main-layout";
 import CategoriesShowcase from "@/components/layouts/showcase/categories-showcase";
 import ShowcaseTable from "@/components/layouts/showcase/showcase-table";
+import Navbar, { items } from "@/components/common/navbar";
+import useSWR from "swr";
+import { ethers } from "ethers";
 import { useWalletAddress } from "@/hooks/use-wallet-address";
-import { useState } from "react";
+import { erc721Abi } from "@/lib/abis";
 
 export default function Showcase() {
   const [selectedCategory, setSelectedCategory] = useState<string>("");
-  const { address, isConnected } = useWalletAddress();
+  const { address } = useWalletAddress()
+  const [ page, setPage ] = useState(0);
+
+  const { data } = useSWR( address ? `SHOWCASE_${page}` : null, async () => {
+    const owner = "0xA7C1d79C7848c019bCb669f1649459bE9d076DA3"
+      const provider = new ethers.JsonRpcProvider(
+          "https://testnet-rpc.monad.xyz", 
+          10143                            
+      )   
+
+      const contract = new ethers.Contract(
+          "0x06D235F0D8ff77c499A6F647a4d44636acf8584A", // địa chỉ đã cho
+          erc721Abi,
+          provider,
+      )
+
+      const uriCollection = await contract.contractURI()
+      console.log(uriCollection)
+
+      const isERC721 = await contract.supportsInterface("0x80ac58cd")
+      const isERC721Metadata = await contract.supportsInterface("0x5b5e139f")
+      const isERC1155 = await contract.supportsInterface("0xd9b67a26")
+      console.log(isERC721, isERC721Metadata, isERC1155)
+      
+      const tokenIds = await contract.tokensOfOwner(owner)
+
+      const tokens: Array<any> = []
+      await Promise.all(tokenIds.map(async (tokenId: number) => {
+          const uri = await contract.tokenURI(tokenId)
+          tokens.push({
+              tokenId: Number(tokenId),
+              uri,
+          })
+      }))
+
+      return tokens
+  })
+
+  console.log(data)
 
   const handleCategoryChange = (category: string) => {
     setSelectedCategory(category);
   };
 
   return (
-    <div className="space-y-6">
-      <div className="flex flex-col sm:flex-row sm:items-center sm:justify-between gap-4">
-        <CategoriesShowcase
-          selectedCategory={selectedCategory}
-          onCategoryChange={handleCategoryChange}
-        />
+    <div className="min-h-screen flex flex-col items-center justify-center px-2">
+      <Navbar navbarItems={items} />
+      <MainLayout
+        headerIcon="/gifs/llamao_majestic_run.gif"
+        text="Showcase"
+        subHeader={false}
+        className="w-full h-full p-2 sm:p-4 lg:p-6"
+        wrapperClassName="w-full max-w-sm sm:max-w-md md:max-w-2xl"
+      >
+        <div className="space-y-6">
+          <CategoriesShowcase
+            selectedCategory={selectedCategory}
+            onCategoryChange={handleCategoryChange}
+          />
 
-        {/* Toggle for showing only user's items */}
-        {/* {isConnected && (
-              <div className="flex items-center gap-2">
-                <label className="flex items-center gap-2 cursor-pointer">
-                  <input
-                    type="checkbox"
-                    checked={showOnlyMyItems}
-                    onChange={handleToggleMyItems}
-                    className="rounded border-gray-300 text-blue-600 shadow-sm focus:border-blue-500 focus:ring-blue-500"
-                  />
-                  <span className="text-sm font-medium text-gray-700">
-                    My Items Only
-                  </span>
-                </label>
-              </div>
-            )} */}
-      </div>
-
-      <ShowcaseTable
-        category={selectedCategory || undefined}
-        userAddress={isConnected ? address : undefined}
-        showOnlyMyItems={true}
-        className="animate-in fade-in duration-300"
-        itemsPerPage={9}
-      />
+          <ShowcaseTable
+            category={selectedCategory || undefined}
+            className="animate-in fade-in duration-300"
+          />
+        </div>
+      </MainLayout>
     </div>
   );
 }
