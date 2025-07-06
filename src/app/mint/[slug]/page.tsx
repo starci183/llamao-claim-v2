28027b41691127b29c6f26949ed6e43b21b41cd1
minhnhat166 <minhnhatphan2003@gmail.com>
2025-07-06 14:10:08 +0700

    Add book tag SVG and integrate into MintPage component
    - Added a new SVG file for the book tag (`book_tag_llamao.svg`) to the images directory.
    - Imported the `Image` component from Next.js in the `MintPage`.
    - Updated the layout of the MintPage to include the book tag image, positioning it with absolute positioning for better visual alignment.

--- src/app/mint/[slug]/page.tsx
+++ src/app/mint/[slug]/page.tsx
@@ -5,6 +5,7 @@ import { useContract, type NftMetadata } from "@/hooks/use-contract";
 import { useEffect, useState } from "react";
 import MintButton from "./components/mint-button";
 import MintContent from "./components/mint-content";
+import Image from "next/image";
 
 export default function MintPage() {
   const { contractURI } = useContract();
@@ -35,11 +36,20 @@ export default function MintPage() {
     >
       <div className="flex items-center justify-center overflow-hidden w-full h-full">
         <div
-          className="w-full h-full flex items-center justify-center bg-no-repeat bg-center bg-cover pointer-events-none aspect-[4/3]"
+          className="w-full h-full flex items-center justify-center bg-no-repeat bg-center bg-cover pointer-events-none aspect-[4/3] relative"
           style={{
             backgroundImage: "url(/images/llamao_web_openbook.svg)",
           }}
         >
+          <div className="absolute top-0 left-0 transform translate-x-[5%] md:translate-x-[35%] md:translate-y-[1%] lg:translate-y-[10%] pointer-events-none">
+            <Image
+              alt="book tag"
+              src="/images/book_tag_llamao.svg"
+              width={240}
+              height={240}
+              className="w-[160px] sm:w-[200px] md:w-[220px] lg:w-[240px] h-auto"
+            />
+          </div>
           <div className="flex w-[95%] md:w-[80%] h-full items-center justify-center pointer-events-auto">
             {nftMetadata && <MintContent nftMetadata={nftMetadata} />}
           </div>
